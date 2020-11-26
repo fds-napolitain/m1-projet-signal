3539b1fc1214e36c3660e3ce0563af279f22053f
Maxime Boucher <mxboucher@gmail.com>
2020-11-26 14:12:09 +0100

    update

--- main.cpp
+++ main.cpp
@@ -4,14 +4,27 @@
 
 using namespace std;
 
-#define FREQ_ECHANTILLONAGE 44100
-#define FREQ_LA 440
+#define ENDLINE "\n"
 
-// incrémente par demi ton, 12 pour un octave
+#define CHANNELS_NB 1
+#define FREQ_ECHANTILLONAGE 44100
+#define FREQ_A4 440
+
+/**
+ * Renvoit une note avec une tonalité changée (par demi ton)
+ * @param freq fréquence en hertz
+ * @param i nombre de demi ton (positif ou négatif)
+ * @return freq fréquence en hertz
+ */
 double incrementSemiTone(double freq, double i) {
 	return freq * pow((pow(2, 1/12)), i);
 }
 
+/**
+ *
+ * @param signal
+ * @return
+ */
 unsigned char doubleToUnsignedChar(double signal) {
 	if (signal > 255) {
 		return (unsigned char) 255;
@@ -21,6 +34,22 @@ unsigned char doubleToUnsignedChar(double signal) {
 	return (unsigned char) floor(signal);
 }
 
+/**
+ *
+ * @param data8
+ * @return
+ */
+double unsignedCharToDouble(unsigned char data8) {
+	return (double) data8;
+}
+
+/**
+ *
+ * @param signal
+ * @param partie_reelle
+ * @param partie_imaginaire
+ * @param N
+ */
 void DFT(double *signal, double *partie_reelle, double *partie_imaginaire, int N) {
 	double val, multiple ;
 	double deux_pi_N = 2.0 * M_PI / (double)N ;
@@ -46,17 +75,17 @@ void DFT(double *signal, double *partie_reelle, double *partie_imaginaire, int N
 	}
 }
 
-void dft()
 
 int main() {
 	/**
-	 * Créer un La 440
+	 * Création d'un accord La majeur à partir du La440
 	 */
+	cout << "Création du signal de A4 majeur sur 6s." << ENDLINE;
 	Wave la440;
-	long int data_nb = 50000;
+	int duree = 6;
+	long int data_nb = FREQ_ECHANTILLONAGE*duree;
 	unsigned char data8[data_nb];
-	short channel_nb = 1;
-	int duree = 6; // secondes
-	la440 = Wave(data8, FREQ_ECHANTILLONAGE*duree, channel_nb, FREQ_ECHANTILLONAGE);
+	la440 = Wave(data8, FREQ_ECHANTILLONAGE*duree, CHANNELS_NB, FREQ_ECHANTILLONAGE);
+	la440.read("sons/la440.wav");
 	return 0;
 }
