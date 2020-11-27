f29d922e2561ba542c63a14784d1ccab1eb3e29d
Maxime Boucher <mxboucher@gmail.com>
2020-11-27 11:47:14 +0100

    suppression utils inutiles

--- utils.cpp
+++ utils.cpp
@@ -1,18 +1,4 @@
-#include <cmath>
 #include "utils.h"
-
-double unsignedCharToDouble(unsigned char data8) {
-	return (double) data8;
-}
-
-unsigned char doubleToUnsignedChar(double signal) {
-	if (signal > 255) {
-		return (unsigned char) 255;
-	} else if (signal < 0) {
-		return (unsigned char) 0;
-	}
-	return (unsigned char) floor(signal);
-}
 
 void Normalisation(double *signal, int N) {
 	double mini, maxi, delta ;
--- utils.h
+++ utils.h
@@ -1,20 +1,6 @@
 #ifndef M1_PROJET_SIGNAL_UTILS_H
 #define M1_PROJET_SIGNAL_UTILS_H
 
-/**
- * Signal en 8 bits (amplitude de 0 à 255 avec 127 pour 0)
- * @param signal
- * @return
- */
-unsigned char doubleToUnsignedChar(double signal);
-
-/**
- * Signal en 8 bits (amplitude de 0 à 255 avec 127 pour 0)
- * @param data8
- * @return
- */
-double unsignedCharToDouble(unsigned char data8);
-
 void Normalisation(double *signal, int N);
 
 #endif //M1_PROJET_SIGNAL_UTILS_H
