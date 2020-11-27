df4a4043e4ad4f2605236541a9fb283a7976694d
Maxime Boucher <mxboucher@gmail.com>
2020-11-27 17:52:36 +0100

    Tone.cpp parfait!

--- Tone.cpp
+++ Tone.cpp
@@ -1,37 +1,42 @@
 #include <cmath>
+#include <iostream>
 #include "Tone.h"
 
+using namespace std;
+
 Tone::Tone(char *tone) {
-	double freq = 440.0; // A4 default
+	freq = 440.0;
 	int coef = 0;
-	if (tone[1] != '4') {
-		coef += 12 * ((int) tone[1] - 4);
+	int multiplier[] = {-9, -7, -5, -4, -2, 0, 2};
+	char* current = (char*) "A4";
+	if (tone[0] == 'C') { // changer de note
+		coef += multiplier[0];
+	} else if (tone[0] == 'D') {
+		coef += multiplier[1];
+	} else if (tone[0] == 'E') {
+		coef += multiplier[2];
+	} else if (tone[0] == 'F') {
+		coef += multiplier[3];
+	} else if (tone[0] == 'G') {
+		coef += multiplier[4];
+	} else if (tone[0] == 'A') {
+		coef += multiplier[5];
+	} else if (tone[0] == 'B') {
+		coef += multiplier[6];
 	}
-	switch (tone[0]) {
-		case 'C':
-			coef -= 9;
-			break;
-		case 'D':
-			coef -= 7;
-			break;
-		case 'E':
-			coef -= 5;
-			break;
-		case 'F':
-			coef -= 4;
-			break;
-		case 'G':
-			coef -= 2;
-			break;
-		case 'B':
-			coef += 2;
-			break;
+	for (int i = 0; i < 7; ++i) {
+		multiplier[i] -= coef;
 	}
+	if (tone[1] != current[1]) { // changer d'octave
+		coef += 12 * ((int) tone[1] - (int) current[1]);
+	}
+	current = tone;
+	cout << coef;
 	if (coef != 0) {
 		incrementSemiTone(freq, (double) coef);
 	}
 }
 
 void Tone::incrementSemiTone(double freq, double i) {
-	freq = freq * pow(pow(2.0, (1.0/12.0)), i);
+	this->freq = freq * pow(pow(2.0, (1.0/12.0)), i);
 }
